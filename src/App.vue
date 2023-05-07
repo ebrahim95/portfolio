6244d7c1b88317061e278a43748b635bc2763ebc
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-05-06 20:52:00 -0500

    first commit but edited some things

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import { loadStarsPreset } from "tsparticles-preset-stars";
+import { FullScreen } from 'tsparticles-engine';
+
+const particlesInit = async engine => {
+  await loadStarsPreset(engine);
+};
+
+const particlesLoaded = async container => {
+  console.log("Particles container loaded", container);
+};
+const particlesOptions = {
+  preset: "stars",
+}
+const message = ref('Portfolio Page')
+
+interface other {
+  name: string
+}
+const other_message: other = reactive({
+  name: "ebrahim"
+})
+</script>
+
+<template>
+  <!-- <div>
+    <Particles id="tsparticles" :particlesInit="particlesInit" :options="particlesOptions" />
+  </div>-->
+  <div>
+    <h1> Ebrahim's Portfolio</h1>
+  </div>
+</template>
+<!-- TODO: need to add about about -->
+
+<!-- TODO: need to add projects  -->
+
+
+<!-- TODO: need to fix particles js  -->
+<style scoped></style>
