f35206ab3c7b9bea02661290566b80eed2e1783b
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-05-07 00:03:00 -0500

    added some text and used prettier

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import { ref, reactive } from 'vue'
-import { loadStarsPreset } from "tsparticles-preset-stars";
-import { FullScreen } from 'tsparticles-engine';
+import { loadStarsPreset } from 'tsparticles-preset-stars'
+import { FullScreen } from 'tsparticles-engine'
 
-const particlesInit = async engine => {
-  await loadStarsPreset(engine);
-};
+const particlesInit = async (engine) => {
+  await loadStarsPreset(engine)
+}
 
-const particlesLoaded = async container => {
-  console.log("Particles container loaded", container);
-};
+const particlesLoaded = async (container) => {
+  console.log('Particles container loaded', container)
+}
 const particlesOptions = {
-  preset: "stars",
+  preset: 'stars'
 }
 const message = ref('Portfolio Page')
 
@@ -19,7 +19,7 @@ interface other {
   name: string
 }
 const other_message: other = reactive({
-  name: "ebrahim"
+  name: 'ebrahim'
 })
 </script>
 
@@ -28,13 +28,45 @@ const other_message: other = reactive({
     <Particles id="tsparticles" :particlesInit="particlesInit" :options="particlesOptions" />
   </div>-->
   <div>
-    <h1> Ebrahim's Portfolio</h1>
+    <h1>Ebrahim's Portfolio</h1>
+    <div>
+      <h2>HOW I STARTED</h2>
+      <p>
+        I started web development in May 2022 by volunteering at an organization to develop a new
+        website. At first, I knew little about creating a website. I used a landing page software
+        called Carrd to develop the website. It was quite a simple software and creating a web page
+        was not too difficult.
+      </p>
+
+      <h2>PROGRESSION</h2>
+      <p>
+        I started to look deeper into web development and stumbled upon Webflow. I looked into and
+        started learning it through Webflow university. After developing a basic skillset, I started
+        to develop free websites for individuals in my discord community.
+      </p>
+      <h2>CURRENTLY,</h2>
+      <p>
+        I am well-versed in the MERN Stack - MongoDB, Express.js, React.js, and Node.js. I can also
+        use Typescript. I aim to be a Front-End Developer with Full-Stack Awareness.
+      </p>
+    </div>
+
+    <div>
+      <h1>BACKGROUND</h1>
+    </div>
+
+    <div>
+      <h1>PERSONAL PROJECTS</h1>
+    </div>
+
+    <div>
+      <h1>VALUES</h1>
+    </div>
   </div>
 </template>
 <!-- TODO: need to add about about -->
 
 <!-- TODO: need to add projects  -->
-
 
 <!-- TODO: need to fix particles js  -->
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Particles from "vue3-particles";
+import Particles from 'vue3-particles'
 import './assets/index.css'
 
 createApp(App).use(Particles).mount('#app')
