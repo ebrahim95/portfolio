de4f094731d96cffd2a6339b6dbbc43dedb3bc08
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-05-17 21:30:06 -0500

    added styles, refactored text, and configured daisy UI

--- src/App.vue
+++ src/App.vue
@@ -12,89 +12,119 @@ const other_message: other = reactive({
 </script>
 
 <template>
-  <div class="font-mono flex justify-center items-center flex-col gap-2 min-h-full">
-    <h1 class="font-si">Ebrahim's Portfolio</h1>
-    <div>
-      <h2>HOW I STARTED</h2>
-      <p>
-        I started web development in May 2022 by volunteering at an organization to develop a new
-        website. At first, I knew little about creating a website. I used a landing page software
-        called Carrd to develop the website. It was quite a simple software and creating a web page
-        was not too difficult.
-      </p>
+  <div id="main-background" class="prose font-mono mx-auto min-h-full p-10 min-w-full">
+    <div class="mx-auto 2xl:w-6/12 lg:w-8/12 flex flex-col items-center">
 
-      <h2>PROGRESSION</h2>
-      <p>
-        I started to look deeper into web development and stumbled upon Webflow. I looked into and
-        started learning it through Webflow university. After developing a basic skillset, I started
-        to develop free websites for individuals in my discord community.
-      </p>
-      <h2>CURRENTLY,</h2>
-      <p>
-        I am well-versed in the MERN Stack - MongoDB, Express.js, React.js, and Node.js. I can also
-        use Typescript. I aim to be a Front-End Developer with Full-Stack Awareness.
-      </p>
-    </div>
+      <h1>Ebrahim's Portfolio</h1>
+      <section>
 
-    <div>
-      <h1>BACKGROUND</h1>
-    </div>
+        <h2 class="text-center">BACKGROUND</h2>
+        <h3>HOW I STARTED</h3>
+        <p>
+          I started web development in May 2022 by volunteering at an organization to develop a new
+          website. At first, I knew little about creating a website. I used a landing page software
+          called Carrd to develop the website. It was quite a simple software and creating a web page
+          was not too difficult.
+        </p>
 
-    <div>
-      <h1>PERSONAL PROJECTS</h1>
-      <div>
-        <h2>Phonebook Application</h2>
+        <h3>PROGRESSION</h3>
         <p>
-          A simple application that functions a phonebook list, to you may remember your phone
-          number
+          I started to look deeper into web development and stumbled upon Webflow. I looked into and
+          started learning it through Webflow university. After developing a basic skillset, I started
+          to develop free websites for individuals in my discord community.
         </p>
-        <p>Node.js, React, MongoDB</p>
-      </div>
-      <div>
-        <h2>Country Information Application</h2>
+        <h3>CURRENTLY,</h3>
         <p>
-          A application to find information about your a country + including the weather of the
-          capital city
+          I am well-versed in the MERN Stack - MongoDB, Express.js, React.js, and Node.js. I can also
+          use Typescript. I aim to be a Front-End Developer with Full-Stack Awareness.
         </p>
-        <!-- TODO:use chatgpt to summarize the history of the country -->
-        <p>Restful API, React, Node</p>
-      </div>
-      <div>
-        <h2>Bloglist Application</h2>
-        <p>
-          A application to store you favorite blogs posts where other users can like and comment on
-          the blog post
-        </p>
-        <p>React, Node.js, Redux</p>
-      </div>
-      <div>
-        <h2>Patient Diagnosis Application</h2>
-        <p>Store your patient records in this application including the type of Diagnosis</p>
-        <p>React, Typescript, Material UI</p>
-      </div>
-    </div>
+      </section>
+      <section class="mb-6">
+        <h2 class="text-center">PERSONAL PROJECTS</h2>
+        <div class="flex flex-row flex-wrap justify-center gap-6 mx-auto 2xl:justify-start">
+          <div class="card card-normal bg-base-300 w-80 card-bordered border-slate-100">
+            <div class="card-body">
+              <h2 class="card-title">Phonebook Application</h2>
+              <p>
+                A simple application that functions a phonebook list, to you may remember your phone
+                number
+              </p>
 
-    <div>
-      <h1>VALUES</h1>
-      <div>
-        <h2>WALKING UPWARDS</h2>
-        <p>Since perfection does not exist, I do my best to continously improve.</p>
-      </div>
-      <div>
-        <h2>ADAPTATION</h2>
-        <p>Better ways of doing things should be adpated to ensure progession</p>
-      </div>
-      <div>
-        <h2>CRAWL FALL, WALK</h2>
-        <p>
-          As Kids we crawl, stand up, fall, and at some point walk. That's the approach I take when
-          I'm learning something new.
-        </p>
-      </div>
-      <div>
-        <h2>ACE</h2>
-        <p>No matter what the project is - we should always try to get it right.</p>
-      </div>
+              <p>Node.js, React, MongoDB</p>
+              <a href="https://www.google.com/"
+                class="btn btn-primary font-semibold text-slate-900 no-underline">Github</a>
+              <a href="https://www.google.com/" class="btn btn-secondary"> Demo </a>
+
+            </div>
+
+          </div>
+          <div class="card card-normal bg-base-300 w-80 card-bordered border-slate-100">
+            <div class="card-body">
+              <h2 class="card-title">Country Information Application</h2>
+              <p>
+                A application to find information about your country + including the weather of the
+                capital city
+              </p>
+              <p>Restful API, React, Node</p>
+              <a class="btn btn-primary"> Github </a>
+              <a class="btn btn-secondary"> Demo </a>
+
+              <!-- TODO:use chatgpt to summarize the history of the country -->
+            </div>
+          </div>
+          <div class="card card-normal bg-base-300 w-80 card-bordered border-slate-100">
+            <div class="card-body">
+              <h2 class="card-title">Bloglist Application</h2>
+              <p>
+                A application to store you favorite blogs posts where other users can like and comment on
+                the blog post
+              </p>
+              <p>React, Node.js, Redux</p>
+              <a class="btn btn-primary"> Github </a>
+              <a class="btn btn-secondary"> Demo </a>
+            </div>
+          </div>
+          <div class="card card-normal bg-base-300 w-80 card-bordered border-slate-100">
+            <div class="card-body">
+              <h2 class="card-title">Patient Diagnosis Application</h2>
+              <p>Store your patient records in this application including the type of Diagnosis</p>
+              <p>React, Typescript, Material UI</p>
+              <a class="btn btn-primary"> Github </a>
+              <a class="btn btn-secondary"> Demo </a>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <h2 class="text-center">VALUES</h2>
+        <div class="grid grid-row-2 gap-6 md:grid-cols-2 ">
+          <div>
+            <kbd class="kbd kbd-lg text-slate-100 font-bold">WALKING UPWARDS</kbd>
+
+            <p>Since perfection does not exist, I do my best to continously improve.</p>
+          </div>
+          <div>
+
+            <kbd class="kbd kbd-lg text-slate-100 font-bold">ADAPTATION</kbd>
+            <p>Better ways of doing things should be adpated to ensure progession</p>
+          </div>
+          <div>
+
+            <kbd class="kbd kbd-lg text-slate-100 font-bold">CRAWL, FALL, WALK</kbd>
+            <p>
+              As Kids we crawl, stand up, fall, and at some point walk. That's the approach I take when
+              I'm learning something new.
+            </p>
+          </div>
+          <div>
+
+            <kbd class="kbd kbd-lg text-slate-100 font-bold">ACE</kbd>
+            <p>No matter what the project is - we should always try to get it right.</p>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
