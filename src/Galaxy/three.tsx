05922cdff9d9fd83e498117638b7a39030d0457d
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-12 00:05:51 -0500

    feat: add the first code of lines to use three.js

---
+++ src/Galaxy/three.tsx
@@ -0,0 +1,13 @@
+import { Canvas } from "@react-three/fiber"
+
+export default function Galaxy() {
+  return (
+    < Canvas >
+      <mesh>
+        <boxGeometry>
+        </boxGeometry>
+      </mesh>
+    </Canvas >
+  )
+}
+
