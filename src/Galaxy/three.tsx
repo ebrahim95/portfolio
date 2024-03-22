cf21a12c038f770dbe6a3a31782c6a167c440088
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-22 11:39:39 -0500

    feat: created galaxy component and imported it into app.tsx

--- src/Galaxy/three.tsx
+++ src/Galaxy/three.tsx
@@ -1,12 +1,14 @@
 import { Canvas } from "@react-three/fiber"
+// import { OrbitControls } from "@react-three/drei"
 
 export default function Galaxy() {
   return (
-    < Canvas >
+    <Canvas>
+      <ambientLight />
       <mesh>
-        <boxGeometry>
-        </boxGeometry>
+        <boxGeometry />
       </mesh>
+      {/* <OrbitControls dampingFactor={0.05} minDistance={1} maxDistance={16384} screenSpacePanning={false} maxPolarAngle={(Math.PI / 2) - (Math.PI / 360)} /> */}
     </Canvas >
   )
 }
