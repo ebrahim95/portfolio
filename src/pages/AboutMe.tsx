574318acaccd5e29926622cf464ef4f4e4bdc997
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-26 00:26:26 -0500

    feat: added content for about me page

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,6 +1,39 @@
 
 export default function AboutMe() {
   return (
-    <></>
+    <>
+      <div>
+        <h3>
+          HOW I STARTED
+        </h3>
+        <p>
+          I started web development in May 2022 by volunteering at an organization to develop a new website.
+          At first, I knew little about creating a website.
+          I used a landing page software called Carrd to develop the website.
+          It was quite a simple software and creating a web page was not too difficult.
+        </p>
+      </div>
+      <div>
+        <h3>
+          PROGESSION
+        </h3>
+        <p>
+          I started to look deeper into web development and stumbled upon Webflow.
+          I looked into and started learning it through Webflow university.
+          After developing a basic skillset,
+          I started to develop free websites for individuals in my discord community.
+          From then on, I started to dive deeper into the world of website development.         </p>
+      </div>
+      <div>
+        <h3>
+          CURRENTLY,
+        </h3>
+        <p>
+          Currently, I'm proficient in React.js, Svelte.js, Node.js, and Express.js.
+          I'm also quite comfortable working with Typescript and creating designs using Tailwind CSS.
+          Lately, I've been diving into the world of Robotics.
+          My main goal is to become a Full-Stack Developer with a keen eye for Front-End development.         </p>
+      </div>
+    </>
   )
 }
