50bbe05be101a6e7d8505987251dd7c842ea42c5
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-26 00:27:28 -0500

    chore: increased thinkness of border

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ function App() {
   return (
     <div>
       <ParticlesBackground />
-      <div className="mx-auto border-2 p-1 w-9/12">
+      <div className="mx-auto border-4 p-1 w-9/12">
         <Navbar />
-        <h1>My Portfolio</h1>
+        <h1 className="">My Portfolio</h1>
         <div className="card">
           <p>
             Edit <code>src/App.tsx</code> and save to test HMR
