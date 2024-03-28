32b84afee8fecc225fd30e86ab252e4a610e501b
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-28 16:51:23 -0500

    feat: added styles and added font

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,11 +1,11 @@
 
 export default function AboutMe() {
   return (
-    <>
+    <div className="m-2 text-black w-5/12 mx-auto border-double border-4 border-black p-4">
       <div>
-        <h3>
+        <h2 className="roboto-slab">
           HOW I STARTED
-        </h3>
+        </h2>
         <p>
           I started web development in May 2022 by volunteering at an organization to develop a new website.
           At first, I knew little about creating a website.
@@ -14,9 +14,9 @@ export default function AboutMe() {
         </p>
       </div>
       <div>
-        <h3>
+        <h2 className="roboto-slab">
           PROGESSION
-        </h3>
+        </h2>
         <p>
           I started to look deeper into web development and stumbled upon Webflow.
           I looked into and started learning it through Webflow university.
@@ -25,15 +25,15 @@ export default function AboutMe() {
           From then on, I started to dive deeper into the world of website development.         </p>
       </div>
       <div>
-        <h3>
+        <h2 className="roboto-slab">
           CURRENTLY,
-        </h3>
+        </h2>
         <p>
           Currently, I'm proficient in React.js, Svelte.js, Node.js, and Express.js.
           I'm also quite comfortable working with Typescript and creating designs using Tailwind CSS.
           Lately, I've been diving into the world of Robotics.
           My main goal is to become a Full-Stack Developer with a keen eye for Front-End development.         </p>
       </div>
-    </>
+    </div>
   )
 }
