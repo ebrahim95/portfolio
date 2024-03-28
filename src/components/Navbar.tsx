aad140b2f7722f3c6b5c80489710d628f59aefd6
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-28 16:50:28 -0500

    chore: changed css styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 //TODO Add Icons with three.js
 
 
-const button = "ring-2 ring-white ring-offset-2 ring-offset-black bg-white text-black rounded-sm m-2 p-2 grow"
+const button = "ring-2 ring-black ring-offset-2 ring-offset-white bg-black text-white rounded-sm m-2 p-2 grow"
 
 function Navbar() {
 
   return (
-    <nav className="flex justify-evenly border-2 w-full h-16">
+    <nav className="flex justify-evenly w-5/12 h-16">
       <button className={`${button}`}>Home</button>
       <button className={`${button}`}>About Me :)</button>
       <button className={`${button}`}>Projects</button>
