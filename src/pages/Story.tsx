7bc15ee47fe40b31ee2be3a0442a66b15c716dfe
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-28 23:43:39 -0500

    chore: changed file name and added a title

---
+++ src/pages/Story.tsx
@@ -0,0 +1,44 @@
+
+export default function Story() {
+  return (
+    <section id="story">
+      <div className="bg-indigo-500/75 border-black border-4 w-4/12 h-5/12 mx-auto mb-2 p-4">
+        <h1 className="text-black">My Story</h1>
+      </div>
+      <div className="bg-indigo-500/50 text-black w-4/12 mx-auto border-solid border-4 border-black p-4">
+        <div className="m-2">
+          <h2 className="roboto-slab">
+            HOW I STARTED
+          </h2>
+          <p>
+            I started web development in May 2022 by volunteering at an organization to develop a new website.
+            At first, I knew little about creating a website.
+            I used a landing page software called Carrd to develop the website.
+            It was quite a simple software and creating a web page was not too difficult.
+          </p>
+        </div>
+        <div className="m-2">
+          <h2 className="roboto-slab">
+            PROGESSION
+          </h2>
+          <p>
+            I started to look deeper into web development and stumbled upon Webflow.
+            I looked into and started learning it through Webflow university.
+            After developing a basic skillset,
+            I started to develop free websites for individuals in my discord community.
+            From then on, I started to dive deeper into the world of website development.         </p>
+        </div>
+        <div className="m-2">
+          <h2 className="roboto-slab">
+            CURRENTLY,
+          </h2>
+          <p>
+            Currently, I'm proficient in React.js, Svelte.js, Node.js, and Express.js.
+            I'm also quite comfortable working with Typescript and creating designs using Tailwind CSS.
+            Lately, I've been diving into the world of Robotics.
+            My main goal is to become a Full-Stack Developer with a keen eye for Front-End development.         </p>
+        </div>
+      </div>
+    </section>
+  )
+}
