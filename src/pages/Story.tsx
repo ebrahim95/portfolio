d008853821b31545138f6abf328c85ae7a3e42c5
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-31 19:18:46 -0500

    chore: changed a few styles

--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -1,12 +1,12 @@
 
 export default function Story() {
   return (
-    <section id="story">
-      <div className="bg-indigo-500/75 border-black border-4 w-4/12 h-5/12 mx-auto mb-2 p-4">
-        <h1 className="text-black">My Story</h1>
+    <section id="story" className="bg-white/75 w-5/12 h-5/12 mt-10 mx-auto">
+      <div className="mb-2">
+        <h1 className="text-black roboto-slab">Story</h1>
       </div>
-      <div className="bg-indigo-500/50 text-black w-4/12 mx-auto border-solid border-4 border-black">
-        <div className="p-2 border-b-4 border-black">
+      <div className="text-black">
+        <div className="mb-5">
           <h2 className="roboto-slab">
             HOW I STARTED
           </h2>
@@ -17,18 +17,19 @@ export default function Story() {
             It was quite a simple software and creating a web page was not too difficult.
           </p>
         </div>
-        <div className="p-2 border-b-4 border-black">
+        <div className="mb-5">
           <h2 className="roboto-slab">
-            PROGESSION
+            PROGESSION,
           </h2>
           <p>
             I started to look deeper into web development and stumbled upon Webflow.
             I looked into and started learning it through Webflow university.
             After developing a basic skillset,
             I started to develop free websites for individuals in my discord community.
-            From then on, I started to dive deeper into the world of website development.         </p>
+            From then on, I started to dive deeper into the world of website development.
+          </p>
         </div>
-        <div className="p-2">
+        <div className="mb-5">
           <h2 className="roboto-slab">
             CURRENTLY,
           </h2>
@@ -36,7 +37,8 @@ export default function Story() {
             Currently, I'm proficient in React.js, Svelte.js, Node.js, and Express.js.
             I'm also quite comfortable working with Typescript and creating designs using Tailwind CSS.
             Lately, I've been diving into the world of Robotics.
-            My main goal is to become a Full-Stack Developer with a keen eye for Front-End development.         </p>
+            My main goal is to become a Full-Stack Developer with a keen eye for Front-End development.
+          </p>
         </div>
       </div>
     </section>
