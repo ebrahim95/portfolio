5b7070b20b8c25c9e0020da329e6f60441cdde53
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-31 19:20:16 -0500

    feat: added project card data and imported project card

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,29 @@ import './App.css'
 import Navbar from './components/Navbar.tsx';
 import './components/Particles'
 import ParticlesBackground from './components/Particles.tsx';
+import ProjectCard from './components/ProjectCard.tsx';
 import Story from './pages/Story.tsx';
 //TODO need to add a small aboutme, add project cards, and values
 const text_style = "ring-2 ring-black ring-offset-2 ring-offset-white bg-black text-white rounded-sm mr-3 mb-3 p-1 "
+const text_for_project_card = [{
+  heading: "Tailwind CSS Generator",
+  detail: "A Tailwind CSS component generator that helps you prototype quickly.",
+  stack: ["Svelte", "Typescript", "Tailwind CSS"],
+  git_link: "www.gooogog.com",
+  demo_link: "www.pop.com"
+}, {
+  heading: "Bloglist Application ",
+  detail: "A application to store you favorite blogs posts where other users can like and comment on the blog post.",
+  stack: ["React", "Node.JS", "Redux"],
+  git_link: "www.gooogog.com",
+  demo_link: "www.pop.com"
+}, {
+  heading: "Gmail AI Summarizer",
+  detail: "Takes all your emails and summarizes into a summary.",
+  stack: ["Python", "Gemini", "RAG"],
+  git_link: "www.gooogog.com",
+  demo_link: "www.pop.com"
+}]
 function App() {
   return (
     <main className='font-sans'>
@@ -32,6 +52,13 @@ function App() {
             <text className={`${text_style}`}>MongoDB</text>
             <text className={`${text_style}`}>RestAPI</text>
           </div>
+        </section>
+        <section className="w-5/12 mx-auto mb-2">
+          <h1 className="text-black roboto-slab">Projects</h1>
+          {text_for_project_card.map(({ heading, detail, stack, git_link, demo_link }) => {
+            return (
+              <ProjectCard heading={heading} detail={detail} stack={stack} git_link={git_link} demo_link={demo_link} />)
+          })}
         </section>
         <Story />
       </div >
