ed819377ca18f6899aaaeeed16b58608a7454a36
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-04-03 01:06:27 -0500

    chore + style: added more information and changed styles

--- src/App.tsx
+++ src/App.tsx
@@ -67,10 +67,12 @@ function App() {
           })}
         </section>
         <Story />
-        <footer className="fixed bg-indigo-300 border-black p-3 border-4 flex justify-center items-center w-full bottom-0">
-          <button>Github</button>
+        <footer className="fixed bg-indigo-300 border-black p-3 border-4 flex gap-2 justify-center items-center w-full bottom-0">
+          <button><a href="">Github</a></button>
           <h1 className='text-black'>|</h1>
-          <button>LinkedIn</button>
+          <button><a href="">LinkedIn</a></button>
+          <h1 className='text-black'>|</h1>
+          <button><a href="">Twitter</a></button>
 
         </footer>
       </div >
