962907b5a33721e3279a0a2f96d92e756cfa919b
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-04-03 00:55:48 -0500

    style: center the elements when the screen is small

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const button = "mr-3"
 function Navbar() {
 
   return (
-    <nav className="flex justify-end items-center w-full h-16">
+    <nav className="flex xl:justify-end justify-center items-center w-full h-16">
       <button className={`${button}`}>Home</button>
       <button className={`${button}`}>About Me :)</button>
       <button className={`${button}`}>Projects</button>
