cfc5991d876d82ce8778f9b1ac19251def4388db
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-04-04 17:03:24 -0500

    chore: added links

--- src/App.tsx
+++ src/App.tsx
@@ -73,11 +73,11 @@ function App() {
         </div>
 
         <footer className="fixed bg-indigo-300 border-black p-3 border-4 flex gap-2 justify-center items-center w-full bottom-0">
-          <button><a href="">Github</a></button>
+          <button><a href="https://github.com/ebrahim95">Github</a></button>
+          <h1 className="text-black">|</h1>
+          <button><a href="https://www.linkedin.com/in/ebrahim-haji/">LinkedIn</a></button>
           <h1 className='text-black'>|</h1>
-          <button><a href="">LinkedIn</a></button>
-          <h1 className='text-black'>|</h1>
-          <button><a href="">Twitter</a></button>
+          <button><a href="https://twitter.com/ebstheace">Twitter</a></button>
 
         </footer>
       </div >
